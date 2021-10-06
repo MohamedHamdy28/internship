399554bd6d52f7a1dc562d0b0b366a6fbf4a499e
MohamedHamdy28 <m.abdelhamid@innopolis.university>
2021-10-07 01:06:23 +0300

    initial output

--- dataeng/main.py
+++ dataeng/main.py
@@ -2,9 +2,19 @@ import os
 import csv
 
 with open("output.csv","w") as output:
-    fieldnames=['first_name','last_name','birthts','image_path']
+    fieldnames = ['first_name', ' last_name', ' birthts', 'image_path']
     # first save the data of the users
-    csv_writer = csv.DictWriter(output, fieldnames=fieldnames, delimiter=",")
-    csv_writer.writeheader()
-    directory = os.fsencode("")
-    for filename in os.listdir()
+    output_writer = csv.DictWriter(output, fieldnames=fieldnames, delimiter=",")
+    output_writer.writeheader()
+    directory = r'D:\University\intenships\Data engineer internship tasks\internship\dataeng\02-src-data'
+    for file in os.listdir(directory):
+        filename = os.fsencode(file)
+        if file.endswith(".csv"):
+            filedir=directory+directory[2]+file;
+            with open(filedir,"r") as data:
+                data_reader=csv.DictReader(data)
+                for line in data_reader:
+                    output_writer.writerow(line)
+
+
+
