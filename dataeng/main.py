82678d6cf7a3794a4e6010fd4a20be0d96e5ef0f
MohamedHamdy28 <m.abdelhamid@innopolis.university>
2021-10-07 01:12:50 +0300

    main

--- dataeng/main.py
+++ dataeng/main.py
@@ -6,11 +6,11 @@ with open("output.csv","w") as output:
     # first save the data of the users
     output_writer = csv.DictWriter(output, fieldnames=fieldnames, delimiter=",")
     output_writer.writeheader()
-    directory = r'D:\University\intenships\Data engineer internship tasks\internship\dataeng\02-src-data'
+    directory ="02-src-data"
     for file in os.listdir(directory):
         filename = os.fsencode(file)
         if file.endswith(".csv"):
-            filedir=directory+directory[2]+file;
+            filedir=directory+'/'+file;
             with open(filedir,"r") as data:
                 data_reader=csv.DictReader(data)
                 for line in data_reader:
